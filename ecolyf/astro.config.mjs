6b2d9c61642316f432319fff534fb9cd97947aad
Nandagopal K <nandagopalng7@gmail.com>
2026-04-12 20:34:14 +0530

    Conent change

--- ecolyf/astro.config.mjs
+++ ecolyf/astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     // @ts-expect-error - Bypassing Vite plugin type mismatch between Astro's internal Vite and Tailwind
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://eco-lyf.com',
+  integrations: [sitemap()]
 });
